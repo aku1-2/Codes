fb244ffdfd9e5a1fee5db1ccaf915adcfa5ceada
akanksha <168823584+aku1-2@users.noreply.github.com>
2025-12-21 16:01:01 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (96.18%) - LeetSync

--- FindTheDifference/Easy/3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
+++ FindTheDifference/Easy/3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
@@ -1,36 +1,22 @@
 class Solution {
 public:
     int maxFrequencyElements(vector<int>& nums) {
-        int n = nums.size();
-        int maxfreq = 0;
-        int result = 0;
-
-        // Step 1: Find the maximum frequency
-        for (int i = 0; i < n; i++) {
-            int count = 0;
-            for (int j = 0; j < n; j++) {
-                if (nums[j] == nums[i]) {
-                    count++;
-                }
-            }
-            if (count > maxfreq) {
-                maxfreq = count;
-            }
-        }
-
-        // Step 2: Add up all elements that have maxfreq
-        for (int i = 0; i < n; i++) {
-            int count = 0;
-            for (int j = 0; j < n; j++) {
-                if (nums[j] == nums[i]) {
-                    count++;
-                }
-            }
-            if (count == maxfreq) {
-                result++; // count every occurrence
-            }
+     unordered_map<int, int> freq;
+     int result=0;
+     int maxfreq=0;
+     
+     for(int i=0; i<nums.size();i++){
+         int num=nums[i];
+         freq[num]++;
+         if (freq[num] > maxfreq)
+             maxfreq = freq[num];
+     }
+     // Sum up all elements that have the maximum frequency
+        for (auto i = freq.begin(); i!= freq.end(); i++) {
+            if (i->second == maxfreq)
+                result += i->second;
         }
 
         return result;
-    }
+ }
 };
