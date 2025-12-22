2dd9d9b120902db752798c5149c33f535b5df55a
akanksha <168823584+aku1-2@users.noreply.github.com>
2025-12-22 17:08:08 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (59.19%) - LeetSync

---
+++ FindTheDifference/Easy/152-maximum-product-subarray/maximum-product-subarray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int maxprod = nums[0];
+        int minprod = nums[0];
+        int result = nums[0];
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]<0)
+            swap(maxprod,minprod);
+            maxprod=max(nums[i],nums[i]*maxprod);
+            minprod=min(nums[i],nums[i]*minprod);
+            result = max(result,maxprod);
+        }
+        return result;
+    }
+};
