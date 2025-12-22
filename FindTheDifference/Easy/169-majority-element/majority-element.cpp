63d8b0f958774ccfe14bd26e0a0406e810f1b829
akanksha <168823584+aku1-2@users.noreply.github.com>
2025-12-22 18:26:38 +0530

    Time: 0 ms (100.00%) | Memory: 28.2 MB (27.99%) - LeetSync

--- FindTheDifference/Easy/169-majority-element/majority-element.cpp
+++ FindTheDifference/Easy/169-majority-element/majority-element.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int maxcountelement=0;
+        /*int maxcountelement=0;
         int count=0;
         for(int i=0;i<nums.size();i++){
             for(int j=0;j<nums.size();j++){
@@ -14,6 +14,16 @@ public:
             }
             count=0;
         }
-        return maxcountelement;
+        return maxcountelement;*/
+
+//Boyar-Moore Voting Algoritm
+        int count=0;
+        int candidate;
+        for(int i=0;i<nums.size();i++){
+            if(count==0) candidate=nums[i];
+            if(nums[i]==candidate) count++;
+            else count--;
+        }
+        return candidate;
     }
 };
