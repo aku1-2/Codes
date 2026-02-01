9c8a331002b318628f8eee6740257657866fc0d9
akanksha <168823584+aku1-2@users.noreply.github.com>
2026-02-01 22:14:01 +0530

    Time: 12 ms (31.55%) | Memory: 19 MB (77.15%) - LeetSync

---
+++ FindTheDifference/Easy/437-path-sum-iii/path-sum-iii.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int count=0;
+   void dfs(TreeNode* root, long long targetSum){
+        if(root==nullptr ) return;
+        
+        if(targetSum==root->val){ 
+           count ++;
+        }
+        
+            dfs(root->left,targetSum-root->val);
+            dfs(root->right,targetSum-root->val);
+            
+        
+        
+   }
+    int pathSum(TreeNode* root, int targetSum) {
+        
+        if(root==nullptr) return 0;
+        dfs(root,targetSum);
+        pathSum(root->left,targetSum);
+        pathSum(root->right,targetSum);
+        return count;
+    }
+};
