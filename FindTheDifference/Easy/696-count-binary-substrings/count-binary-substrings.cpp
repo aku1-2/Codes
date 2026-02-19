7e3123acad29dc5fdc4109f4c7313f9dfb9cf2c9
akanksha <168823584+aku1-2@users.noreply.github.com>
2026-02-20 00:31:47 +0530

    Time: 3 ms (67.83%) | Memory: 17 MB (7.46%) - LeetSync

---
+++ FindTheDifference/Easy/696-count-binary-substrings/count-binary-substrings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int countBinarySubstrings(string s) {
+       
+        vector<int> groups;
+        int count = 1;
+
+        for (int i = 1; i < s.size(); i++) {
+            if (s[i] != s[i - 1]) {
+                groups.push_back(count);
+                count = 1;
+            } else {
+                count++;
+            }
+        }
+        groups.push_back(count);
+
+        int ans = 0;
+        for (int i = 1; i < groups.size(); i++) {
+            ans += min(groups[i - 1], groups[i]);
+        }
+        return ans;
+    }
+};
