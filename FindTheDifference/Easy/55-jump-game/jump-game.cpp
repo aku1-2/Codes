4a96bc2348917aa5b4ed76ab55efb60627150a18
akanksha <168823584+aku1-2@users.noreply.github.com>
2026-03-30 14:03:46 +0530

    Time: 0 ms (100.00%) | Memory: 52.4 MB (56.81%) - LeetSync

---
+++ FindTheDifference/Easy/55-jump-game/jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxR=0;
+      for(int i=0;i<nums.size();i++){
+        if(i>maxR) return false;
+        else
+        maxR= max(maxR, i+nums[i]);
+      }
+
+      return true;
+    }
+};
