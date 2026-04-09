77f7f54d2221841dc6b5c918a8e005a0b7cb51d4
Akanksha <168823584+aku1-2@users.noreply.github.com>
2026-04-09 18:09:11 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (86.88%) - LeetSync

--- FindTheDifference/Easy/226-invert-binary-tree/invert-binary-tree.cpp
+++ FindTheDifference/Easy/226-invert-binary-tree/invert-binary-tree.cpp
@@ -16,11 +16,17 @@ public:
         if(root==nullptr){
             return nullptr;
         }
-        TreeNode* root1 = new TreeNode(root->val);
+       /* TreeNode* root1 = new TreeNode(root->val);
         root1->left = invertTree(root->right);
         root1->right = invertTree(root->left);
 
-        return root1;
+        return root1;*/
+        //Swapping technique
+        TreeNode* left = invertTree(root->left);
+        TreeNode* right = invertTree(root->right);
+        root->left=right;
+        root->right=left;
+        return root;
 
     }
 };
