b8308c653cae4001bd83e7db0e5bb9cb6dfedae4
Akanksha Giri <168823584+aku1-2@users.noreply.github.com>
2026-06-09 15:32:30 +0530

    Time: 384 ms (7.13%) | Memory: 313.1 MB (5.47%) - LeetSync

---
+++ FindTheDifference/Easy/438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        if(p.size()>s.size())
+        return {};
+        vector<int> freq1(26,0);
+        vector<int> ans;
+        for(char c : p){
+            freq1[c-'a']++;
+        }
+        for(int i =0; i<= s.size()-p.size();i++){
+            string a= s.substr(i,p.size());
+             vector<int> freq2(26,0);
+             for(char c : a)
+                 freq2[c-'a']++;
+
+                 if(freq2==freq1)
+                   ans.push_back(i);
+
+        }
+        return ans;
+    }
+};
