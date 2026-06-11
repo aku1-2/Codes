c324d115a5644e91871ab5d2672cd4485c3fb95e
Akanksha Giri <168823584+aku1-2@users.noreply.github.com>
2026-06-11 23:02:04 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (50.05%) - LeetSync

---
+++ FindTheDifference/Easy/394-decode-string/decode-string.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<int> st1;
+        stack<string> st2;
+        string ans = "";
+        int num = 0;
+
+        for (char ch : s) {
+            if (isdigit(ch)) {
+                num = num * 10 + (ch - '0');
+            }
+
+            else if (ch == '[') {
+                st1.push(num);
+                st2.push(ans);
+                num = 0;
+                ans = "";
+            }
+
+            else if (ch == ']') {
+                int k = st1.top();
+                st1.pop();
+                string temp = st2.top();
+                st2.pop();
+
+                while (k--) {
+                    temp += ans;
+                }
+                ans = temp;
+            }
+            else {
+                ans += ch;
+        }
+        }
+        return ans;
+    }
+};
