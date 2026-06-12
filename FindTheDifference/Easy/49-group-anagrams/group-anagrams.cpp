162c870facba6f43478ec6bbe42f4c7d5d447c7f
Akanksha Giri <168823584+aku1-2@users.noreply.github.com>
2026-06-12 21:32:02 +0530

    Time: 16 ms (59.33%) | Memory: 25.1 MB (67.47%) - LeetSync

---
+++ FindTheDifference/Easy/49-group-anagrams/group-anagrams.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>> m;
+        vector<vector<string>>ans;
+        for (string s: strs) {
+            string k= s;
+            sort(k.begin(),k.end());
+            
+            m[k].push_back(s);
+        }
+        for (auto &i: m) {
+            ans.push_back(i.second);
+        }
+        return ans;
+    }
+};
