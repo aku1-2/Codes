8f7366e59edd9f41e5023b7df76e3e8ed8d13b73
Akanksha Giri <168823584+aku1-2@users.noreply.github.com>
2026-06-17 21:15:29 +0530

    Time: 2 ms (47.25%) | Memory: 12.8 MB (61.24%) - LeetSync

--- FindTheDifference/Easy/496-next-greater-element-i/next-greater-element-i.cpp
+++ FindTheDifference/Easy/496-next-greater-element-i/next-greater-element-i.cpp
@@ -3,15 +3,15 @@ public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         stack<int> s;
         unordered_map<int,int> m;
-        for(int i = nums2.size()-1; i >= 0; i--){
-            while(!s.empty() && s.top() < nums2[i]){
+         vector<int> ans;
+        for(int i = nums2.size()-1; i>= 0; i--){
+            while(!s.empty() && s.top()< nums2[i]){
                 s.pop();
             }
 
-            m[nums2[i]] = s.empty() ? -1 : s.top();
+            m[nums2[i]] = s.empty()? -1 :s.top();
             s.push(nums2[i]);
         }
-        vector<int> ans;
         for(int x : nums1){ 
             ans.push_back(m[x]);
         }
