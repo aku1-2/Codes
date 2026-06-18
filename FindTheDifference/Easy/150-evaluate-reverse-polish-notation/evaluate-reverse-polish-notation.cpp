1958b40c2436e7312dd7a1c3a36c73eefda8f001
Akanksha Giri <168823584+aku1-2@users.noreply.github.com>
2026-06-18 16:13:39 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (65.49%) - LeetSync

---
+++ FindTheDifference/Easy/150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> s;
+        for(int i=0; i<tokens.size();i++){
+            if(tokens[i] == "+" || tokens[i] == "-" || tokens[i] == "*" || tokens[i] == "/"){
+                int b = s.top();
+                s.pop();
+                int a = s.top();
+                s.pop();
+                if(tokens[i] == "+")
+                  s.push(a+b);
+                if(tokens[i] == "-")
+                  s.push(a-b);
+                if(tokens[i] == "*")
+                  s.push(a*b);
+                if(tokens[i] == "/")
+                  s.push(a/b);
+            }
+            else{
+                s.push(stoi(tokens[i]));
+            }
+        }
+        return s.top();
+    }
+};
