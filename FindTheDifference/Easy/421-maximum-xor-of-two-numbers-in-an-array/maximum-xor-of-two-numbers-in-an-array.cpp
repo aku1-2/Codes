35ab50043befd464f159c3f76f120aa81521a0fd
Akanksha Giri <168823584+aku1-2@users.noreply.github.com>
2026-07-02 16:55:53 +0530

    Time: 624 ms (48.97%) | Memory: 183.9 MB (96.61%) - LeetSync

---
+++ FindTheDifference/Easy/421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.cpp
@@ -0,0 +1,67 @@
+class TrieNode {
+public:
+    TrieNode* child[2];
+
+    TrieNode() {
+        child[0] = nullptr;
+        child[1] = nullptr;
+    }
+};
+
+class Trie {
+public:
+    TrieNode* root;
+
+    Trie() {
+        root = new TrieNode();
+    }
+
+    void insert(int num) {
+        TrieNode* node = root;
+
+        for (int i = 31; i >= 0; i--) {
+            int bit = (num >> i) & 1;
+
+            if (node->child[bit] == nullptr)
+                node->child[bit] = new TrieNode();
+
+            node = node->child[bit];
+        }
+    }
+
+    int getMaxXor(int num) {
+        TrieNode* node = root;
+        int maxXor = 0;
+
+        for (int i = 31; i >= 0; i--) {
+            int bit = (num >> i) & 1;
+            int opposite = 1 - bit;
+
+            if (node->child[opposite]) {
+                maxXor |= (1 << i);
+                node = node->child[opposite];
+            } else {
+                node = node->child[bit];
+            }
+        }
+
+        return maxXor;
+    }
+};
+
+class Solution {
+public:
+    int findMaximumXOR(vector<int>& nums) {
+        Trie trie;
+
+        for (int x : nums)
+            trie.insert(x);
+
+        int ans = 0;
+
+        for (int x : nums)
+            ans = max(ans, trie.getMaxXor(x));
+
+        return ans;
+    }
+};
