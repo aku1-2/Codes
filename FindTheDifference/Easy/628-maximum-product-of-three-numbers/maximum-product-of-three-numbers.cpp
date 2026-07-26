d9e5e7d30c164a9e0f9fb8fae805f229c4c199d0
Akanksha Giri <168823584+aku1-2@users.noreply.github.com>
2026-07-26 22:31:07 +0530

    Time: 7 ms (50.69%) | Memory: 31.4 MB (96.80%) - LeetSync

--- FindTheDifference/Easy/628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
+++ FindTheDifference/Easy/628-maximum-product-of-three-numbers/maximum-product-of-three-numbers.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int maximumProduct(vector<int>& nums) {
-        
-        
         sort(nums.begin(),nums.end());
         if(nums.size()==3){
             return nums[0]*nums[1]*nums[2];
